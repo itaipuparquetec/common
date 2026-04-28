56dfda3ad4836df549f43fb2da9ec6c4cf5a542c
Emanuel Victor <emanuel.fonseca@itaipuparquetec.org.br>
2026-04-28 16:59:37 -0300

    MIRROR-1069 ## Extra:
    - Trazendo PageResponse para o domínio, uma vez que a paginação do spring está no domínio, mas a nossa paginação não está.

--- src/test/java/br/org/itaipuparquetec/common/domain/pagination/PageResponseTest.java
+++ src/test/java/br/org/itaipuparquetec/common/domain/pagination/PageResponseTest.java
@@ -1,4 +1,4 @@
-package br.org.itaipuparquetec.common.infrastructure.rest;
+package br.org.itaipuparquetec.common.domain.pagination;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageImpl;
