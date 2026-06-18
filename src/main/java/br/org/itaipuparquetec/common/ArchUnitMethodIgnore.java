76a8d7281c8bfd07ae9047748e011602e85c4386
Emanuel Victor <emanuel.fonseca@itaipuparquetec.org.br>
2026-06-18 10:57:38 -0300

    FSOFT-190 - Criando anotação ArchUnitMethodIgnore; - Subindo versão do pom.xml.

---
+++ src/main/java/br/org/itaipuparquetec/common/ArchUnitMethodIgnore.java
@@ -0,0 +1,8 @@
+package br.org.itaipuparquetec.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+public @interface ArchUnitMethodIgnore {
+}
---
+++ src/test/java/br/org/itaipuparquetec/common/ArchUnitMethodIgnoreTest.java
@@ -0,0 +1,43 @@
+package br.org.itaipuparquetec.common;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArchUnitMethodIgnoreTest {
+
+    @Test
+    void mustBeAnAnnotationType() {
+        assertThat(ArchUnitMethodIgnore.class.isAnnotation()).isTrue();
+    }
+
+    @Test
+    void mustTargetOnlyMethods() {
+        final var target = ArchUnitMethodIgnore.class.getAnnotation(Target.class);
+
+        assertThat(target).isNotNull();
+        assertThat(target.value()).containsExactly(ElementType.METHOD);
+    }
+
+    @Test
+    void mustNotHaveExplicitRetentionPolicy() {
+        final var retention = ArchUnitMethodIgnore.class.getAnnotation(Retention.class);
+
+        assertThat(retention).isNull();
+    }
+
+    @ArchUnitMethodIgnore
+    void annotatedMethod() {
+    }
+
+    @Test
+    void mustBeApplicableToMethods() throws NoSuchMethodException {
+        final var method = ArchUnitMethodIgnoreTest.class.getDeclaredMethod("annotatedMethod");
+
+        assertThat(method).isNotNull();
+    }
+}
