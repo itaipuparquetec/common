4d5dd7fc7367dee455b977c5cd5a38ed3b6e1855
Emanuel Victor <emanuel.fonseca@itaipuparquetec.org.br>
2026-06-18 11:04:44 -0300

    FSOFT-190 - Criando anotação ArchUnitMethodIgnore; - Subindo versão do pom.xml.

--- src/main/java/br/org/itaipuparquetec/common/ArchUnitMethodIgnore.java
+++ src/main/java/br/org/itaipuparquetec/common/ArchUnitMethodIgnore.java
@@ -3,6 +3,10 @@ package br.org.itaipuparquetec.common;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * Utilize to ignore some methods in arch unit function.
+ * You need to create a technical-debt every time you use it.
+ */
 @Target(ElementType.METHOD)
 public @interface ArchUnitMethodIgnore {
 }
